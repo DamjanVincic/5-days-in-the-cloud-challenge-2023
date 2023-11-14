1b35f6ecf07fea63c35df585e9eae562189a8151
Damjan Vincic <damjanvincic2003@gmail.com>
2023-11-14 10:54:01 +0100

    Change database to in memory

--- five_days_in_the_cloud_challenge/settings.py
+++ five_days_in_the_cloud_challenge/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'five_days_in_the_cloud_challenge.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': ':memory:',
     }
 }
 
